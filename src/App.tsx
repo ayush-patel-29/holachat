68cf7e46ab7309c475e4763b35fd188184842bd6
ayush-patel-29 <ayushpatel.org@gmail.com>
2024-10-13 17:42:38 +0530

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
   return (
-    <div>
-      This is my App
+    <div className='h-[100vh] w-full flex flex-col justify-between'>
+      <nav className='text-white h-16 flex items-center '>
+        <a href="/" className='h-full flex items-center gap-4'>
+          <img src="/logo.jpg" alt="logo" className='h-20 w-20' />
+          <h1 className='text-2xl font-bold '>Hola Chat</h1>
+        </a>
+      </nav>
+      <div className='text-white h-full flex text-center border-2'>
+        <h1 className='w-full text-4xl'>Hola Chat is Fast</h1>
+      </div>
     </div>
   )
 }
